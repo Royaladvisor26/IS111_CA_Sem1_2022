f3a85da5b8635077a563327e87598c292cd89ad4
smaranan.2019 <smaranan.2019@smu.edu.sg>
2022-10-12 16:05:36 +0800

    updated

--- Session_3/q4.py
+++ Session_3/q4.py
@@ -1,3 +1,30 @@
-def encode(string):
+from re import I
+
+
+def encode(n):
     # Your code here
-    return string
+    count = 0
+    output = ""
+    
+    for i in range(len(n)):
+        if i == 0:
+            count = 1
+            continue
+        if n[i] == n[i-1]:
+            count += 1
+        else:
+            if n[i].isnumeric():
+                output += str(count) + "["+n[i-1]+"]" 
+            else:
+                output += str(count) + n[i-1]
+            count = 1
+    if n[i].isnumeric():
+        output += str(count) + "["+n[i]+"]" 
+    else:       
+        output += str(count) + n[i]
+    return output
+
+
+
+econded = encode("aa22b888cc")
+print(econded)
